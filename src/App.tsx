afd6e85942776176400409243bee14305eb04a33
Josias Pereira <josias.ngpereira@gmail.com>
2024-05-08 22:25:00 -0300

    feat: add `tailwindcss ` and setup colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <p>Hello World!</p>;
+  return (
+    <h1 className="text-white90 text-5xl bg-text inter">
+      Hello world!
+    </h1>
+  )
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'white': '#fff',
+      'white60': '#ffffff99', 
+      'white90': '#ffffffe6', 
+      'text': '#252527',
+      'text85': '#252527d9', 
+      'text50': '#25252780', 
+      'background-color': '#F2E9E4',
+      'primary': '#AA906B',
+      'primary60': '#aa906b99', 
+      'alert': '#FB4D3D',
+      'warning': '#E7BB41',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
