8f58e8a82651678c003c49a96b80fd0c1f61f657
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-05-08 21:08:54 -0300

    feat: create `envs` file to manager all environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,23 +1,21 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
-const env = import.meta.env
+import { envs } from '../config';
 
 const firebaseConfig = {
-  apiKey: env.VITE_FIREBASE_API_KEY,
-  authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: env.VITE_FIREBASE_DATABASE_URL,
-  projectId: env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: env.VITE_FIREBASE_APP_ID
+  apiKey: envs.apiKey,
+  authDomain: envs.authDomain,
+  databaseURL: envs.databaseURL,
+  projectId: envs.projectId,
+  storageBucket: envs.storageBucket,
+  messagingSenderId: envs.messagingSenderId,
+  appId: envs.appId,
 };
 
-console.log(firebaseConfig)
-
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-const database = getDatabase(app)
+const auth = getAuth(app);
+const database = getDatabase(app);
 
-export { app, database, auth }
+export { app, database, auth };
